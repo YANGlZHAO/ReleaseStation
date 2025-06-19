e18eb46818870e2a3958b33fdfcb162519175e3a
YlZ <1134538225@qq.com>
2025-06-19 14:38:04 +0800

    333

--- App.vue
+++ App.vue
@@ -1,29 +1,24 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-			// 设置根字体大小，1rem = 20px
-			// const docEl = document.documentElement
-			// const resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
-			// const recalc = function() {
-			// 	const clientWidth = docEl.clientWidth || 375 // 默认375
-			// 	docEl.style.fontSize = 20 * (clientWidth / 375) + 'px' // 基于375设计稿
-			// }
-			// window.addEventListener(resizeEvt, recalc, false)
-			// document.addEventListener('DOMContentLoaded', recalc, false)
-			// recalc()
+export default {
+  mounted() {
+    if (process.env.UNI_PLATFORM === 'app-plus') {
+      const closeSplash = () => {
+        this.$nextTick(() => {
+          setTimeout(() => {
+            if (typeof plus !== 'undefined') {
+              plus.navigator.closeSplashscreen();
+              uni.hideLoading();
+            }
+          }, 1000);
+        });
+      };
 
-			if (process.env.UNI_PLATFORM === 'app-plus') {
-				if (typeof plus !== 'undefined') {
-					plus.navigator.closeSplashscreen(); // 关闭启动页
-					uni.hideLoading(); // 关闭loading（防止遗留）
-				}
-			}
-		},
-	}
+      if (typeof plus !== 'undefined') {
+        closeSplash();
+      } else {
+        document.addEventListener('plusready', closeSplash);
+      }
+    }
+  }
+}
 </script>
-
-
-<style>
-
-</style>
